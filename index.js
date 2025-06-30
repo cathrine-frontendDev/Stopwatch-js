f2cefe0939121f780774d1b5c1df0e0ea21d044f
Cathrine <cathrinekamasho@gmail.com>
2025-06-30 14:02:26 +0200

    Update index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const display = document.getElementById("display");
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
+
+function start() {
+
+}
+
+function stop() {
+
+}
+
+function reset() {
+
+}
